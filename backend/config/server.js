7e62159f11836207c8a07dee89c8d7b1eed16cfa
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-04 13:23:33 +0800

    Initial Commit: Git and Strapi Installation

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd39b6c1faf32798bf6634bb86456662a'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5a61c842-05bd-4df0-9eea-11ffa905885b'
+};
